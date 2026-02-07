93c2161d26b9edd80f5dcd8a157a8376eba387b3
Sebastian Bobos <7sebastianbo11@gmail.com>
2026-02-07 17:56:40 -0500

    add convolutional layer and constructor

--- Deliverable 3/Checkers/src/main/java/main/Main.java
+++ Deliverable 3/Checkers/src/main/java/main/Main.java
@@ -1,9 +1,15 @@
 package main;
 
+import model.ConvolutionalLayer;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        ConvolutionalLayer layer = new ConvolutionalLayer(32, 4, 3, 3);
+
+        System.out.println("DD");
     }
 }
--- Deliverable 3/Checkers/src/main/java/model/ConvolutionalLayer.java
+++ Deliverable 3/Checkers/src/main/java/model/ConvolutionalLayer.java
@@ -1,6 +1,33 @@
 package model;
 
+import java.util.Random;
+
 public class ConvolutionalLayer {
+   public class BoardSize {
+       int width;
+       int height;
+   }
+
     private double[][][][] kernels;
 
+    public ConvolutionalLayer(int numInAndOut, int channels, int width, int height) {
+        this.kernels = new double[numInAndOut][channels][width][height];
+
+        Random random = new Random();
+
+        for (int i = 0; i < kernels.length; i++) {
+            for (int j = 0; j < kernels[i].length; j++) {
+                for (int k = 0; k < kernels[i][j].length; k++) {
+                    for (int l = 0; l < kernels[i][j][k].length; l++) {
+                        kernels[i][j][k][l] = random.nextGaussian();
+                    }
+                }
+            }
+        }
+    }
+
+    public ConvolutionalLayer(int channels, BoardSize board, int numOut, String filePath) {
+        this.kernels = new double[numOut][channels][board.width][board.height];
+    }
+    
 }
