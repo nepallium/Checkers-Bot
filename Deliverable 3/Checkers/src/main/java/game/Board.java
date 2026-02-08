284e554b317d2254f9c580ffdbb93ce68f18c811
DevManDan1178 <l3r0d4niel@gmail.com>
2026-02-08 14:27:56 -0500

    + board is now 8x8 array + functions for action space

--- Deliverable 3/Checkers/src/main/java/game/Board.java
+++ Deliverable 3/Checkers/src/main/java/game/Board.java
@@ -1,15 +1,17 @@
 package game;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Board {
 
-    public int[] cells;
-    public int currentPlayer;
+    public int[][] cells; //[row index][column index], row are counted from white side to black side
+    public boolean whiteToMove;
 
-    int[][] RED_DIRS = { {-1, -1}, {-1, +1} };
-    int[][] BLACK_DIRS = { {+1, -1}, {+1, +1} };
+    private static int MAN_VALUE = 1;
+    private static int KING_VALUE = 2;
 
     int[][] KING_DIRS = {
             {-1, -1}, {-1, +1},
@@ -17,55 +19,120 @@ public class Board {
     };
 
 
-    public Board(int[] cells, int player) {
+    public Board(int[][] cells, boolean whiteToMove) {
         this.cells = cells;
-        this.currentPlayer = player;
+        this.whiteToMove = whiteToMove;
     }
 
     public Board() {
-        this.cells = initializeBoard();
-        this.currentPlayer = 1;
+        this.cells = getStartingBoard();
+        this.whiteToMove = true;
     }
 
-    private int[] initializeBoard() {
-        int[] board = new int[64];
-
-
-        for (int colIdx = 0; colIdx < 8; colIdx += 2) {
-            board[idx(0, colIdx + 1)] = -1;
-            board[idx(1, colIdx)] = -1;
+    private static int[][] getStartingBoard() {
+        //empty board
+        int[][] newCells = new int[8][8];
+        //place pieces
+        for (int rowIdx = 0; rowIdx < 8; rowIdx++) {
+            //no pieces in the middle 2 rows
+            if (rowIdx == 3 || rowIdx == 4) {
+                continue;
+            }
+            //row[0] starts with a valid square and alternates every row, place pieces space out by 1
+            for (int xIdx = (rowIdx % 2 == 0 ? 0 : 1); xIdx < 8; xIdx += 2) {
+                newCells[rowIdx][xIdx] = rowIdx < 3 ? MAN_VALUE : -MAN_VALUE;
+            }
         }
+        return newCells;
+    }
 
-        for (int colIdx = 0; colIdx < 8; colIdx += 2) {
-            board[idx(6, colIdx + 1)] = 1;
-            board[idx(7, colIdx)] = 1;
+    @Override
+    //The array string is flipped so the board can be perceived through the white player's point of view
+    public String toString() {
+        String str = String.format("%s to move \n", whiteToMove ? "White" : "Black");
+        for (int rowIdx = 7; rowIdx >= 0; rowIdx--) {
+            for (int xIdx = 0; xIdx < 8; xIdx++) {
+                str += String.format("[%4s]", cells[rowIdx][xIdx]);
+            }
+            str += "\n";
         }
-
-        return board;
+        return str;
     }
 
-    public double[] convertToDoubleArray() {
-        return Arrays.stream(cells).asDoubleStream().toArray();
-    }
-
-//    public List<Move> getLegalMoves() {
-//
-//    }
 
     /**
-     * Applies a move to a board and returns a new board
-     * Assumes the move is already legal
-     * @param move a legal move
-     * @return the new board state
+     * Gets the action space of the piece
+     * @param coordinate coordinate of the piece
+     * @param piece the value of the piece (1 for man, 2 for king, positive for white, negative for black)
+     * @param previousCaptures [used for recursion], stores the previous captures made by the piece (if the move follows a capture)
+     * @return a list of possible moves for the piece
      */
-    public Board applyMove(Move move) {
-        cells[move.to] = move.from;
-
-        currentPlayer = -currentPlayer;
-
-        return new Board(cells, currentPlayer);
+    public List<Move> getPieceActionSpace(Coordinate coordinate, int piece, List<Coordinate> previousCaptures) {
+        List<Move> actionSpace = new ArrayList<>();
+        List<Move> pieceMoves = (Math.abs(piece) > 1 ? coordinate.getPossibleKingMoves() : coordinate.getPossibleManMoves(piece > 0));
+        for (Move move : pieceMoves) {
+            Coordinate moveDestination = move.getDestination();
+            if (moveDestination.isInvalid()) {
+                continue;
+            }
+            int destinationPiece = getPieceAt(moveDestination);
+            //if there is no piece on the square, the move is valid
+            if (destinationPiece == 0) {
+                actionSpace.add(move);
+                continue;
+            }
+            Coordinate captureDestination = move.getDestination().addedWith(move.getDeltaCoordinates());
+            //if there is a piece on the square, check if can be captured
+            if (!captureDestination.isInvalid() && !arePiecesSameTeam(destinationPiece, piece)) {
+                List<Coordinate> captures = previousCaptures == null ? new ArrayList<>() : previousCaptures;
+                captures.add(move.getDestination());
+                actionSpace.add(new Move(move.getStart(), captureDestination, captures));
+                //after captures, can move again
+                List<Move> nextActionSpace = getPieceActionSpace(captureDestination, piece, captures);
+                actionSpace.addAll(nextActionSpace);
+            }
+        }
+        return actionSpace;
     }
 
+    /**
+     * Gets the action space for every peace for a player
+     * @param whiteToMove the player to find actions for
+     * @return a list of every possible move
+     */
+    public List<Move> getGlobalActionSpace(boolean whiteToMove) {
+        List<Move> globalActionSpace = new ArrayList<>();
+        //Assuming piece values: Man = 1, King = 2 (+ for white, - for black)
+        for (int row = 0; row < cells.length; row++) {
+            for (int col = 0; col < cells[0].length; col++) {
+                int piece = cells[row][col];
+                //if no piece or piece color is not the color of the player who moves, skip
+                if (piece == 0 || (piece < 0 == whiteToMove)) {
+                    continue;
+                }
+                Coordinate coordinate = new Coordinate(col, row);
+                globalActionSpace.addAll(getPieceActionSpace(coordinate, piece, null));
+            }
+        }
+        return globalActionSpace;
+    }
+
+    public List<Move> getActionSpaceForCoordinate(Coordinate coords) {
+        if (coords.isInvalid()) {
+            System.out.println("Invalid coords" + coords);
+            return new ArrayList<>();
+        }
+        return getPieceActionSpace(coords, getPieceAt(coords), null);
+    }
+
+    public boolean arePiecesSameTeam(int piece1, int piece2) {
+        return piece1 > 0 == piece2 > 0 && !(piece1 == 0 || piece2 == 0);
+    }
+
+    public int getPieceAt(Coordinate coords) {
+        return cells[coords.getYIndex()][coords.getXIndex()];
+    }
+    /* DEPRECATED - board is now 8x8 array
     public static int idx(int r, int c) {
         return r * 8 + c;
     }
@@ -77,7 +144,21 @@ public class Board {
     public static int col(int idx) {
         return idx % 8;
     }
+    */
 
+    /*
+     * DEPRECATED - board is now 8x8 array
+     * Applies a move to a board and returns a new board
+     * Assumes the move is already legal
+     * @param move a legal move
+     * @return the new board state
 
+    public Board applyMove(Move move) {
+    cells[move.to] = move.from;
 
+    currentPlayer = -currentPlayer;
+
+    return new Board(cells, currentPlayer);
+    }
+     */
 }
