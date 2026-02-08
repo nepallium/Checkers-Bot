cbc3b8ecefe4a8afca365812ad15397b774e35ff
DevManDan1178 <l3r0d4niel@gmail.com>
2026-02-08 14:28:17 -0500

    + moves adapted to 8x8 board, also tracks captures

--- Deliverable 3/Checkers/src/main/java/game/Move.java
+++ Deliverable 3/Checkers/src/main/java/game/Move.java
@@ -1,13 +1,58 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Move {
-    int from;
-    int to;
+    private Coordinate start;
+    private Coordinate destination;
+    private List<Coordinate> captures;
 
-    public Move(int from, int to) {
-        this.from = from;
-        this.to = to;
+
+    public Move(Coordinate start, Coordinate destination, List<Coordinate> captures) {
+        this.start = start;
+        this.destination = destination;
+        this.captures = captures;
+    }
+
+    public Move(Coordinate start, Coordinate destination) {
+        this(start, destination, new ArrayList<>());
+    }
+
+    public Coordinate getDeltaCoordinates() {
+        return new Coordinate(destination.getXIndex() - start.getXIndex(), destination.getYIndex() - start.getYIndex());
+    }
+
+    public Coordinate getDestination() {
+        return destination;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("<Start: %s, Destination: %s, Captures:%s>", start, destination, captures);
+    }
+
+    public void setDestination(Coordinate destination) {
+        this.destination = destination;
+    }
+
+    public Coordinate getStart() {
+        return start;
+    }
+
+    public void setStart(Coordinate start) {
+        this.start = start;
+    }
+
+    public List<Coordinate> getCaptures() {
+        return captures;
+    }
+
+    public void setCaptures(List<Coordinate> captures) {
+        this.captures = captures;
+    }
+
+    public boolean isCapture() {
+        return !this.captures.isEmpty();
     }
 }
