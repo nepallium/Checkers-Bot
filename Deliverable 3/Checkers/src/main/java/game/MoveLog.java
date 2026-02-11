6dbeef49de07ee397e88bdb13a1bc23d2adad256
DevManDan1178 <l3r0d4niel@gmail.com>
2026-02-11 13:19:49 -0500

    + now its own class

---
+++ Deliverable 3/Checkers/src/main/java/game/MoveLog.java
@@ -0,0 +1,44 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveLog {
+    private final List<Move> whiteMoves;
+    private final List<Move> blackMoves; //size is either equal to or one less than that of white moves
+    private boolean whiteTurn = true;
+
+    public MoveLog() {
+        this.whiteMoves = new ArrayList<Move>(List.of(new Move()));
+        this.blackMoves = new ArrayList<Move>(List.of(new Move()));
+    }
+
+    /**
+     /**
+     * Adds an action to the log
+     *
+     * @param action   action to add
+     * @param turnOver whether the turn is over or not
+     */
+    public void addActionLog(Action action, boolean turnOver) {
+        List<Move> playerMoves = whiteTurn ? whiteMoves : blackMoves;
+        playerMoves.getLast().addAction(action);
+        if (!turnOver) {
+            return;
+        }
+
+        playerMoves.addLast(new Move());
+        whiteTurn = !whiteTurn;
+        return;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        int blackMoveCount = blackMoves.size();
+        for (int i = 0; i < blackMoveCount; i++) {
+            str += String.format("%s | %s\n", whiteMoves.get(i).toString(), blackMoves.get(i).toString());
+        }
+        return str;
+    }
+}
---
+++ Deliverable 3/Checkers/src/main/java/game/PositionLog.java
@@ -0,0 +1,58 @@
+package game;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PositionLog {
+    private final List<int[][]> recentPositions; //index from most recent to least
+    private final int maxPositionLogs;
+
+    public PositionLog(int maxPositionLogs) {
+        recentPositions = new LinkedList<>();
+        this.maxPositionLogs = maxPositionLogs;
+    }
+
+    /**
+     *  Adds a board position to the log
+     *
+     * @param board board to save position of (deep copies it)
+     */
+    public void addPosition(int[][] board) {
+        int[][] boardCopy = new int[8][8];
+        for (int i = 0; i < 8; i++) {
+            boardCopy[i] = board[i].clone();
+        }
+        recentPositions.addFirst(boardCopy);
+        if (recentPositions.size() > maxPositionLogs) {
+            recentPositions.removeLast();
+        }
+    }
+
+    /**
+     *
+     * @return if there is a draw by repetition (same position reached 3x)
+     *
+     */
+    public boolean checkForDrawByRepetition() {
+        if (recentPositions.size() < 6) {
+            return false;
+        }
+        for (int i = 2; i < Board.DRAW_BY_REPETITION_REPEAT_AMOUNT; i++) {
+            //Check this position equals last position (rewind by 2, because by 1 is last player's turn if there was no double capture [wouldn't be a draw])
+            if (!Arrays.deepEquals(recentPositions.getFirst(), recentPositions.get(i))) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public List<int[][]> getRecentPositions() {
+        return recentPositions;
+    }
+
+    public int getMaxPositionLogs() {
+        return maxPositionLogs;
+    }
+}
