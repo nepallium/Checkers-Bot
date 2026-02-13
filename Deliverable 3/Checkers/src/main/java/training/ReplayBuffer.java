2ff0afe027ba678ac28d42877cd3188143b7d749
nepallium <122187419+nepallium@users.noreply.github.com>
2026-02-13 09:13:41 -0500

    add: ReplayBuffer for sampling minibatches

--- Deliverable 3/Checkers/src/main/java/training/ReplayBuffer.java
+++ Deliverable 3/Checkers/src/main/java/training/ReplayBuffer.java
@@ -1,4 +1,40 @@
 package training;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Stores multiple TrainingExamples and selects random minibatches
+ */
 public class ReplayBuffer {
+    public List<TrainingExample> dataList;
+    private final int MAXSIZE = 10000; // max size for dataList
+    private final Random random = new Random();
+
+
+    public void addNewTrainingData(List<TrainingExample> newExamples) {
+        if (newExamples == null) {
+            return;
+        }
+
+        dataList.addAll(newExamples);
+
+        // clear old examples once MAXSIZE exceeded
+        if (dataList.size() > MAXSIZE) {
+            int amtToRemove = dataList.size() - MAXSIZE;
+
+            dataList.subList(0, amtToRemove).clear();
+        }
+    }
+
+    public List<TrainingExample> sample(int batchSize) {
+        List<TrainingExample> samples = new ArrayList<>(batchSize);
+        for (int i = 0; i < batchSize; i++) {
+            int randIdx = random.nextInt(dataList.size());
+            samples.add(dataList.get(randIdx));
+        }
+
+        return samples;
+    }
 }
