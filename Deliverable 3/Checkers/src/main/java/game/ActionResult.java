41a8ee39a99a87678f0ad0f2ab6b9cd4b1b04158
DevManDan1178 <l3r0d4niel@gmail.com>
2026-02-13 12:19:22 -0500

    + field for promotion

--- Deliverable 3/Checkers/src/main/java/game/ActionResult.java
+++ Deliverable 3/Checkers/src/main/java/game/ActionResult.java
@@ -2,17 +2,23 @@ package game;
 
 public class ActionResult extends Action {
     private final int capturedPiece;
+    private boolean promotion;
 
     public ActionResult(Action action) {
-        this(action, 0);
+        this(action, 0, false);
     }
 
     public ActionResult(Action action, int capturedPiece) {
-        super(action.getStart(), action.getDestination());
-        this.capturedPiece = capturedPiece;
+        this(action, capturedPiece, false);
     }
 
-    public ActionResult(Coordinate start, Coordinate destination, int capturedPiece) {
+    public ActionResult(Action action, int capturedPiece, boolean promotion) {
+        super(action.getStart(), action.getDestination());
+        this.capturedPiece = capturedPiece;
+        this.promotion = promotion;
+    }
+
+    public ActionResult(Coordinate start, Coordinate destination, int capturedPiece, boolean promotion) {
         super(start, destination);
         this.capturedPiece = capturedPiece;
     }
@@ -25,4 +31,9 @@ public class ActionResult extends Action {
     public int getCapturedPiece() {
         return capturedPiece;
     }
+
+    public boolean isPromotion() {
+        return promotion;
+    }
+
 }
