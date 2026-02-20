d8f48f2ab08702565925c2cbd1739b441fdfbf9c
DevManDan1178 <l3r0d4niel@gmail.com>
2026-02-20 08:46:24 -0500

    ~ fixed typos

--- Deliverable 3/Checkers/src/main/java/model/ResidualBlock.java
+++ Deliverable 3/Checkers/src/main/java/model/ResidualBlock.java
@@ -1,11 +1,12 @@
 package model;
+
 import model.ConvolutionalLayer;
 
 public class ResidualBlock {
     ConvolutionalLayer layer1;
     ConvolutionalLayer layer2;
 
-    private postActivationOutput2[][][];
+    private double[][][] postActivationOutput2;
 
     public ResidualBlock(ConvolutionalLayer layer1, ConvolutionalLayer layer2) {
         this.layer1 = layer1;
@@ -18,13 +19,13 @@ public class ResidualBlock {
         x = layer2.forwardNoActivation(x);
         double[][][] out = add3DArrays(x, input);
         for (int i = 0; i < out.length; i++) {
-            for(int j = 0; j < out[i].length; j++) {
+            for (int j = 0; j < out[i].length; j++) {
                 for (int k = 0; k < out[i][j].length; k++) {
                     out[i][j][k] = Activation.relu(out[i][j][k]);
                 }
             }
         }
-        
+
         this.postActivationOutput2 = out;
         return out;
     }
@@ -33,7 +34,7 @@ public class ResidualBlock {
         double[][][] sum = new double[x.length][x[0].length][x[0][0].length];
 
         for (int i = 0; i < x.length; i++) {
-            for(int j = 0; j < x[i].length; j++) {
+            for (int j = 0; j < x[i].length; j++) {
                 for (int k = 0; k < x[i][j].length; k++) {
                     sum[i][j][k] = x[i][j][k] + y[i][j][k];
                 }
@@ -47,7 +48,7 @@ public class ResidualBlock {
         return Math.max(0, x);
     }
 
-        public ConvolutionalLayer getLayer1() {
+    public ConvolutionalLayer getLayer1() {
         return this.layer1;
     }
 
