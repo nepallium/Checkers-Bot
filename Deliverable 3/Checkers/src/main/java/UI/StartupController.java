f0efaef564f5346548df76992367570e5d99481d
DevManDan1178 <l3r0d4niel@gmail.com>
2026-03-04 20:04:10 -0500

    + button actions

--- Deliverable 3/Checkers/src/main/java/UI/StartupController.java
+++ Deliverable 3/Checkers/src/main/java/UI/StartupController.java
@@ -1,4 +1,24 @@
 package UI;
 
+import javafx.event.ActionEvent;
+import main.App;
+
 public class StartupController {
+
+    public void onEasyButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void onMediumButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void onHardButtonAction(ActionEvent actionEvent) {
+    }
+
+
+    public void onPlayButtonAction(ActionEvent actionEvent) {
+        App.setScene("mainGame");
+    }
+
 }
+
+
